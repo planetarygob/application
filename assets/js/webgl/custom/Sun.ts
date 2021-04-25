a67cc6c58b076918c8b73dc9f084d016cb295ae8
Florian Blandin <pro.blandinf@gmail.com>
2021-04-25 19:36:55 +0200

    handle the beginning of quiz restiction

--- assets/js/webgl/custom/Sun.ts
+++ assets/js/webgl/custom/Sun.ts
@@ -1,4 +1,4 @@
-import { Group, Vector3, Object3D } from "three"
+import { Group, Vector3, Object3D, Mesh } from "three"
 import { GLTF } from "three/examples/jsm/loaders/GLTFLoader"
 
 class Sun extends Object3D {
@@ -16,6 +16,21 @@ class Sun extends Object3D {
         this.scale.set(0.05, 0.05, 0.05)
         this.rotation.y = this.rotation.y + Math.PI
         this.visible = false
+
+        if (this.name === 'quiz_sun') {
+            console.log('quiz ok', );
+            this.illuminateQuiz(model)
+        }
+        
+    }
+
+    illuminateQuiz(model: GLTF) {
+        model.scene.traverse((child: any) => {
+            if (child instanceof Mesh) {
+                child.material.opacity = 0.2
+                child.material.transparent = true
+            }
+        })
     }
 }
 
