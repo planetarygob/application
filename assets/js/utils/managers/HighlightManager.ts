6e5aad85142237e2674d5e65cd212812cb1f661d
Florian Blandin <pro.blandinf@gmail.com>
2021-04-29 21:40:23 +0200

    fix composer override

--- assets/js/utils/managers/HighlightManager.ts
+++ assets/js/utils/managers/HighlightManager.ts
@@ -1,9 +1,11 @@
 import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js';
 import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js';
 import { OutlinePass } from 'three/examples/jsm/postprocessing/OutlinePass.js';
-import { WebGLRenderer, PerspectiveCamera, Vector2, Object3D } from 'three';
+import { GammaCorrectionShader } from 'three/examples/jsm/shaders/GammaCorrectionShader';
+import { WebGLRenderer, PerspectiveCamera, Vector2, Object3D, sRGBEncoding, ACESFilmicToneMapping, Color, LinearFilter, RGBAFormat, WebGLRenderTarget } from 'three';
 import { GLEvents } from '../Events';
 import EventBus from '../EventBus';
+import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass';
 
 class HighlightManager {
     private static instance: HighlightManager
@@ -16,10 +18,13 @@ class HighlightManager {
 
         this.renderPass = new RenderPass(scene, camera);
         this.composer.addPass(this.renderPass);
-
+        
         this.outlinePass = new OutlinePass(new Vector2(window.innerWidth, window.innerHeight), scene, camera);
         this.outlinePass.renderToScreen = true
         this.composer.addPass(this.outlinePass);
+
+        this.composer.renderTarget1.texture.encoding = sRGBEncoding
+        this.composer.renderTarget2.texture.encoding = sRGBEncoding
 
         EventBus.on(GLEvents.UPDATE_HIGHLIGHT_MANAGER, () => {
             this.update()
