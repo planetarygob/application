52599d6e452fdbfcd3740ae46187744c292e010d
Lou Batier <batierlou@gmail.com>
2021-04-30 08:48:03 +0200

    wip

--- assets/js/webgl/GL.ts
+++ assets/js/webgl/GL.ts
@@ -5,8 +5,10 @@ import {
 import Stats from '../utils/dev/Stats'
 import EventBus from '../utils/EventBus'
 import { GLEvents } from '../utils/Events'
-import { initGUI } from '../utils/dev/GUIFolders'
-import Tracker from '../utils/dev/Tracker'
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js';
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js';
+import { BokehPass } from 'three/examples/jsm/postprocessing/BokehPass.js';
+import GUI from '../utils/dev/GUI'
 
 interface Size {
     width: number
@@ -21,8 +23,15 @@ class GL {
     size: Size
     canvas: HTMLCanvasElement
     isAnimationMixerRequired: boolean
+    // ------------------------------ BLUR
+    materials: any
+    ojects: any
+    postprocessing: any
+    effectController: any
+    // ------------------------------
 
     constructor() {
+
         Stats.showPanel(0)
         document.body.appendChild(Stats.dom)
 
@@ -41,6 +50,29 @@ class GL {
         this.isAnimationMixerRequired = false
 
         this.listenEvents()
+        
+
+        // ------------------------------ BLUR
+        this.materials = []
+        this.ojects = []
+        this.postprocessing = {}
+
+        this.effectController = {
+            focus: 0,
+            aperture: 10,
+            maxblur: 0.01
+        }
+
+        this.initPostprocessing()
+
+        GUI.add( this.effectController, "focus", -100, 100, 1 ).onChange( this.matChanger.bind(this) )
+        GUI.add( this.effectController, "aperture", 0, 15, 1 ).onChange( this.matChanger.bind(this) )
+        GUI.add( this.effectController, "maxblur", 0.0, 0.01, 0.001 ).onChange( this.matChanger.bind(this) )
+
+        this.matChanger.bind(this)
+
+        // ------------------------------
+
         this.animate()
     }
 
@@ -70,8 +102,39 @@ class GL {
         this.scene.camera.aspect = this.size.width / this.size.height
         this.scene.camera.updateProjectionMatrix()
 
-        this.scene.renderer.setSize(this.size.width, this.size.height)
+        this.scene.renderer.setSize( this.size.width, this.size.height )
+        this.postprocessing.composer.setSize( this.size.width, this.size.height )
     }
+
+    // ------------------------------ BLUR
+
+    initPostprocessing() {
+        const renderPass = new RenderPass( this.scene, this.scene.camera )
+        const bokehPass = new BokehPass( this.scene, this.scene.camera, {
+            focus: this.effectController.focus,
+            aperture: this.effectController.aperture,
+            maxblur: this.effectController.maxblur,
+
+            width: this.size.width,
+            height: this.size.height
+        } )
+
+        const composer = new EffectComposer( this.scene.renderer )
+
+        composer.addPass( renderPass )
+        composer.addPass( bokehPass )
+
+        this.postprocessing.composer = composer
+        this.postprocessing.bokeh = bokehPass
+    }
+
+    matChanger() {
+        this.postprocessing.bokeh.uniforms[ "focus" ].value = this.effectController.focus;
+        this.postprocessing.bokeh.uniforms[ "aperture" ].value = this.effectController.aperture * 0.00001;
+        this.postprocessing.bokeh.uniforms[ "maxblur" ].value = this.effectController.maxblur;
+    }
+
+    // ------------------------------
 
     // ---------------- LIFECYCLE
     // TODO : Rework so that we're not dependent of the user's framerate
@@ -86,6 +149,9 @@ class GL {
     }
 
     render() {
+        // ------------------------------ BLUR
+
+        // ------------------------------
         if (this.clock) {
             EventBus.emit(GLEvents.UPDATE, {
                 elapsedTime: this.clock.getElapsedTime() 
@@ -97,6 +163,9 @@ class GL {
         }
 
         this.scene.renderer.render(this.scene, this.scene.camera)
+
+        this.postprocessing.composer.render()
+
     }
 }
 
