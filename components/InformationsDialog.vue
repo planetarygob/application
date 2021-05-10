a56cf6dc7f0e0f9a9508f124c33a25205e22181e
Lou Batier <batierlou@gmail.com>
2021-05-10 19:00:00 +0200

    working on anim

--- components/InformationsDialog.vue
+++ components/InformationsDialog.vue
@@ -1,47 +1,51 @@
 <template>
-    <div
-        v-if="content"
-        class="container flex flex-col absolute bg-white w-1/5 rounded-xl justify-center items-center px-16 py-12"
-        style="top: 50%; left: 50%; transform: translate(-50%, -50%);"
-        @click="$emit('update:is-displayed', false)">
-        <img 
-            v-if="content.image"
-            :src="`/images/${content.image.name}.png`"
-            :width="content.image.size.width"
-            :height="content.image.size.height">
-        <h2
-            v-if="content.title"
-            class="mt-4 font-black text-xl">
-            {{ content.title }}
-        </h2>
-        <p 
-            v-if="content.text"
-            class="mt-4 text-center text-sm text-center"
-            v-html="content.text" />
-        <button
-            v-if="content.action"
-            class="Button mt-12 text-white font-bold hover:text-#201838 hover:bg-white py-2 rounded-full"
-            style="background-color: #201838; padding-left: 1rem; padding-right: 0.5rem;"
-            @click="back"
-            @mouseenter="hoverButton()"
-            @mouseleave="hoverButton()">
-            <div class="flex flex-row">
-                <span>{{ content.action }}</span>
-                <img 
-                    src="/images/button.png"
-                    width="28px"
-                    height="28px"
-                    class="ml-6" />
-            </div>
-        </button>
-    </div>
+    <transition name="fade">
+        <div
+            v-if="content"
+            class="container flex flex-col absolute bg-white w-1/5 rounded-xl justify-center items-center px-16 py-12"
+            ref="informationsDialog"
+            style="top: 50%; left: 50%; transform: translate(-50%, -50%);"
+            @click="$emit('update:is-displayed', false)">
+            <img 
+                v-if="content.image"
+                :src="`/images/${content.image.name}.png`"
+                :width="content.image.size.width"
+                :height="content.image.size.height">
+            <h2
+                v-if="content.title"
+                class="mt-4 font-black text-xl">
+                {{ content.title }}
+            </h2>
+            <p 
+                v-if="content.text"
+                class="mt-4 text-center text-sm text-center"
+                v-html="content.text" />
+            <button
+                v-if="content.action"
+                class="Button mt-12 text-white font-bold hover:text-#201838 hover:bg-white py-2 rounded-full"
+                style="background-color: #201838; padding-left: 1rem; padding-right: 0.5rem;"
+                @click="back"
+                @mouseenter="hoverButton()"
+                @mouseleave="hoverButton()">
+                <div class="flex flex-row">
+                    <span>{{ content.action }}</span>
+                    <img 
+                        src="/images/button.png"
+                        width="28px"
+                        height="28px"
+                        class="ml-6" />
+                </div>
+            </button>
+        </div>
+    </transition>
 </template>
 
 <script>
 
 import SvgIcon from './SvgIcon.vue'
 import EventBus from '../assets/js/utils/EventBus'
-import { GLEvents, AnimationEvents } from '../assets/js/utils/Events'
+import { GLEvents, AnimationEvents, UIEvents } from '../assets/js/utils/Events'
+import gsap from 'gsap/all'
 
 export default {
     components: {
@@ -64,7 +68,7 @@ export default {
             this.$emit('update:is-displayed', false)
         })
     },
-
+    
     methods: {
         back () {
             this.$emit('update:is-displayed', false)
@@ -79,6 +83,14 @@ export default {
 </script>
 
 <style scoped>
+
+    .fade-enter-active, .fade-leave-active {
+    transition: opacity .5s;
+    }
+    
+    .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+    opacity: 0;
+    }
 
     h2 {
         font-family: 'Soulmaze', sans-serif;
