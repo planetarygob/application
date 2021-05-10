d6259eeed031495b472c11931fca89822986f613
Florian Blandin <pro.blandinf@gmail.com>
2021-05-10 20:51:14 +0200

    main sound -> loop

--- pages/index.vue
+++ pages/index.vue
@@ -158,6 +158,7 @@ export default {
             video.volume = 0.8
             video.onended = function (e) {
                 if (audio) {
+                    audio.loop = true
                     audio.volume = 0.1
                     audio.play()
                 }
@@ -214,7 +215,7 @@ export default {
                 if (planetName !== undefined) {
                     this.sceneryAudio = document.querySelector(`#${planetName}_audio`)
                     if (this.sceneryAudio) {
-                        this.sceneryAudio.volume = 0.15
+                        this.sceneryAudio.volume = 0.1
                         this.sceneryAudio.play()
                     }
                 }
